da697e14d9258e5e7e9eebdeb433911cb78e2dbb
Isero24 <6080755+Isero24@users.noreply.github.com>
2025-04-12 16:32:40 -0400

    Update index.py

--- index.py
+++ index.py
@@ -17,6 +17,8 @@ workbook = pd.read_excel(file_path, sheet_name=None, header=None)
 # Max number of trials that can be selected from each set
 num_of_trials_per_set = 5
 
+# This is just for generating a lot of datasets
+# mainly used to test a lot of iterations to assure that sets are being randomized properly
 for x in range(1,100):
 
     # Generates the random trials from each set
@@ -29,6 +31,7 @@ for x in range(1,100):
         if (len(set(all_excluded_sets)) == len(workbook)):
             print("Failed to generate legal randomization")
             print("resetting randomization")
+            # setting i to 0 restarts the for loop and the randomization process
             i = 0
 
         # intializing
@@ -43,6 +46,9 @@ for x in range(1,100):
             initial_trials = workbook.copy()
             random_trials = pd.DataFrame()
 
+        # generate a setsize randomely
+        # excluded setsizes from randomization are the previous set used and any sets that have reached
+        # their maximum amount of selected sets
         random_set = random_exclude(all_excluded_sets)
         exclude_previous_set = random_set
 
@@ -56,17 +62,21 @@ for x in range(1,100):
         initial_trials[random_set] = initial_trials[random_set].drop(index)
         initial_trials[random_set] = initial_trials[random_set].reset_index(drop=True)
 
-
-
+        # Increase the setsize's max count if it's not at max yet
         if (set_max_count[random_set] < num_of_trials_per_set): 
             set_max_count[random_set] += 1
 
+        # Once a setsize has reached is maximum amount of selections
+        # it is excluded from the randomization process
         if (set_max_count[random_set] == num_of_trials_per_set):
             exclude_max_use_sets.append(random_set)
 
         all_excluded_sets = exclude_max_use_sets + [exclude_previous_set]
 
         i += 1
+
+    # for each trial randomization, create a new sheet for the results
+    # then write the results to an xlsx file
     sheet_name='Random Trials ' + str(x)
     with pd.ExcelWriter("Results.xlsx", engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
         random_trials.to_excel(writer, sheet_name=sheet_name, index=False)
